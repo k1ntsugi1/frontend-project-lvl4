ed58c5b0de339220b873023ef3391d9a8ea22c02
k1ntsugi1 <bmasalimov5@gmail.com>
2022-06-09 21:35:51 +0300

    add autoscroll after new Message

--- src/components/messageField/messageItems/BodyMessageField.jsx
+++ src/components/messageField/messageItems/BodyMessageField.jsx
@@ -1,5 +1,5 @@
 
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
 
 import { selectorsMessages } from '../../../slices/dataMessagesSlice.js';
@@ -10,8 +10,14 @@ export const BodyMessageField = () => {
     const currentActiveChannelId = useSelector( (store) => store.dataChannels.currentChannelId);
     const messages = useSelector(selectorsMessages.selectAll);
     const messagesCurrentChannel = getMessagesCurrentChannel(currentActiveChannelId, messages);
+    const bodyMessageField = useRef();
+
+    useEffect(() => {
+        bodyMessageField.current.scrollTop = bodyMessageField.current.scrollHeight;
+    })
+
     return (
-        <div className='px-5 pb-0 mb-0 flex-grow-1 rounded scrollbar scrollbar-primary'>
+        <div ref={bodyMessageField} className='px-5 pb-0 mb-0 flex-grow-1 rounded scrollbar scrollbar-primary'>
             { (messagesCurrentChannel.length > 0) 
                 && messagesCurrentChannel.map((message) => {
                     return <MessageItem key={message.id} message={message}/>
