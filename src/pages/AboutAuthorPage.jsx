dde9d7513e2d23396c57e39152089fb631371c92
k1ntsugi1 <bmasalimov5@gmail.com>
2022-06-23 13:25:05 +0300

    edit margin

--- src/pages/AboutAuthorPage.jsx
+++ src/pages/AboutAuthorPage.jsx
@@ -28,54 +28,52 @@ const AboutAuthorPage = ({t}) => {
         dispatch(actionsUiNavBar.setNewActivePage({newActivePage: 'about'}))
     }, [])
     return (
-        <div className="my-4 pt-5 d-flex flex-column h-100 shadow border border-success rounded bg-light">
-            <div className="scrollbar2 scrollbar-success">
-            <div className="flex-grow-1">
-                <h3 className="pb-4 text-center">
-                    Привет! Меня зовут Булат и это мой заключительный проект на{' '} 
-                    <a className="link-success" 
-                       href="#" 
-                       onClick={(e) => handlerOpening(e,"https://ru.hexlet.io/")}
-                    >
-                        Hexlet
-                    </a>.
-                </h3>
-                <p className={classNamesP}>
-                    Цель данного проекта - написать real-time приложение (аналог Slack-чата) на React/Redux, 
-                    используя AJAX, REST, websockets, React (с хуками) + Redux (@reduxjs/toolkit) + Formik.
-                </p>
-                <p className={classNamesP}>
-                    Дополнительно использовались следующие пакеты: react-toastify, reactjs-popup, react-dom-router, react-rollbar, react-spinner-loader, yup.
-                </p>
-                <p className={classNamesP}>
-                    Для упрощения создания внешнего вида, в этом проекте применялись библиотеки bootstrap и react-bootstrap.
-                </p>
-                <p className={classNamesP}>
-                    Код серверной части и первоначальная настройка плагинов и webpack предоставлены командой{' '}
-                    <a className="link-success" 
-                       href="#" 
-                       onClick={(e) => handlerOpening(e,"https://ru.hexlet.io/")}
-                    >
-                        Hexlet
-                    </a>.
-                    За мной же стоит вся фронтенд часть.
-                </p>
-                <div className="text-center">
-                    <p>
-                        Здесь будут красивые ссылки-карточки на мои проекты 
+        <div className="my-4 pt-3 d-flex flex-column h-100 shadow border border-success rounded bg-light">
+            <div className="mb-0 flex-grow-1 scrollbar2 scrollbar-success">
+                    <h3 className="pb-4 text-center">
+                        Привет! Меня зовут Булат и это мой заключительный проект на{' '} 
+                        <a  className="link-success" 
+                            href="#" 
+                            onClick={(e) => handlerOpening(e,"https://ru.hexlet.io/")}
+                        >
+                            Hexlet
+                        </a>.
+                    </h3>
+                    <p className={classNamesP}>
+                        Цель данного проекта - написать real-time приложение (аналог Slack-чата) на React/Redux, 
+                        используя AJAX, REST, websockets, React (с хуками) + Redux (@reduxjs/toolkit) + Formik.
                     </p>
-                    <p className="fst-italic text-left">- Какой-то дополнительный и нужный текст</p>
-                </div>
-                <hr/>
-                <h4 className='px-3 lh-base text-center'>
-                    Наша великолепная команда
-                </h4>
-                <div className="d-flex flex-row justify-content-around pb-2">
-                    <img src={MeSecond} className={classnamesMyImg} width="15%" alt="myPhoto"/>
-                    <img src={MeFirst} className={classnamesMyImg} width="15%" alt="myPhoto"/>
-                    <img src={MeThird} className={classnamesMyImg} width="15%" alt="myPhoto"/>
-                </div>
-            </div>
+                    <p className={classNamesP}>
+                        Дополнительно использовались следующие пакеты: react-toastify, reactjs-popup, react-dom-router, react-rollbar, react-spinner-loader, yup.
+                    </p>
+                    <p className={classNamesP}>
+                        Для упрощения создания внешнего вида, в этом проекте применялись библиотеки bootstrap и react-bootstrap.
+                    </p>
+                    <p className={classNamesP}>
+                        Код серверной части и первоначальная настройка плагинов и webpack предоставлены командой{' '}
+                        <a  className="link-success" 
+                            href="#" 
+                            onClick={(e) => handlerOpening(e,"https://ru.hexlet.io/")}
+                        >
+                            Hexlet
+                        </a>.
+                        За мной же стоит вся фронтенд часть.
+                    </p>
+                    <div className="text-center">
+                        <p>
+                            Здесь будут красивые ссылки-карточки на мои проекты 
+                        </p>
+                        <p className="fst-italic text-left">- Какой-то дополнительный и нужный текст</p>
+                    </div>
+                    <hr/>
+                    <h4 className='px-3 lh-base text-center'>
+                        Наша великолепная команда
+                    </h4>
+                    <div className="d-flex flex-row justify-content-around pb-2">
+                        <img src={MeSecond} className={classnamesMyImg} width="15%" alt="myPhoto"/>
+                        <img src={MeFirst} className={classnamesMyImg} width="15%" alt="myPhoto"/>
+                        <img src={MeThird} className={classnamesMyImg} width="15%" alt="myPhoto"/>
+                    </div>
             </div>
             
             
@@ -100,7 +98,7 @@ const AboutAuthorPage = ({t}) => {
                         </span>
                     </div>
                     <div className="fw-bold pe-3">
-                        <img src={GithubImg} width="20px"  alt="GuthubImg"/>
+                        <img src={GithubImg} width="27px"  alt="GuthubImg"/>
                         <span className={classnamesSpan}>
                             Github:{' '}
                             <a className={classnamesLinks} 
